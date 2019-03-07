48bd514b30d0b214018cfe3dc0f824a841fc1e10
swatiAgarwal15 <swati.agarwal_mca18@gla.ac.in>
2019-03-07 19:53:19 +0530

    final commit

--- app/src/main/java/swati/agarwal/MainActivity.java
+++ app/src/main/java/swati/agarwal/MainActivity.java
@@ -1,5 +1,6 @@
 package swati.agarwal;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,12 +21,25 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        final EditText name, message;
+
+        name = findViewById(R.id.editTextName);
+        message = findViewById(R.id.editTextMessage);
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                String s1 = name.getText().toString().trim();
+                String s2 = message.getText().toString().trim();
+                if (s1.equals("") || s2.equals("")) {
+                    Toast.makeText(MainActivity.this, "enter all values", Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent sendMessageIntent = new Intent(MainActivity.this,secondActivity.class);
+                    sendMessageIntent.putExtra("name",s1);
+                    sendMessageIntent.putExtra("message",s2);
+                    startActivity(sendMessageIntent);
+                }
             }
         });
     }
